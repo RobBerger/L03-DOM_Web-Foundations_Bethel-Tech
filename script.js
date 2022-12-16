970a7c2e1cdcdef132d104be03ceb5c2edd59b15
berger <robertberger.dev@gmail.com>
2022-12-16 14:09:52 -0500

    gitByTagName

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ const demoClass = document.getElementsByClassName("item");
 for (i = 0; i < demoClass.length; i++) {
     demoClass[i].style.border = '2px solid orange';
 }
+
+//getByTagName
+const demoTag = document.getElementsByTagName('article');
+console.log(demoTag);
+
+for (i = 0; i < demoTag.length; i++) {
+    demoTag[i].style.border = 'blue dashed';
+}
