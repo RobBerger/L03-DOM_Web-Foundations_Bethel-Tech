cc2c3354ea3bd34219f21b6c192d3236fb29f9c6
berger <robertberger.dev@gmail.com>
2022-12-16 13:21:27 -0500

    getByClassName

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const demoId = document.getElementById("demo");
 console.log(demoId);
 
 demoId.style.background = "yellow";
+
+
+///getByClassName
+const demoClass = document.getElementsByClassName("item");
+for (i = 0; i < demoClass.length; i++) {
+    demoClass[i].style.border = '2px solid orange';
+}
